07f23f77e00b4d57ea4ded214d13b18b5438eac8
Darrel <darrel_cai@hotmail.com>
2020-10-19 18:02:54 +0800

    modified Gemfile

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("chartkick")
-require("chart.js")
-require("Chart.bundle")
+// require("chartkick")
+// require("chart.js")
+// require("Chart.bundle")
 
 
 
