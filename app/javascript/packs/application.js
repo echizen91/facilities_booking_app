a2079a84721f55072703873a419c939392ecb3ae
Darrel <darrel_cai@hotmail.com>
2020-10-20 14:11:48 +0800

    change application.scss location

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,8 @@
 // that code so it'll be compiled.
 
 import "bootstrap"
-import "../stylesheets/application"
+// import "../stylesheets/application"
+import "../../assets/stylesheets/application"
 
 require("@rails/ujs").start()
 require("turbolinks").start()
